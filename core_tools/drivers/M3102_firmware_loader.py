5d0ce16007f96dc9256cfdc334bd7f474c7f3aeb
TUD278427 <TUD278427@tudelft.net>
2020-06-04 11:50:34 +0200

    Generalized the firmware loader. Just copy the paths to a station specific file (pulse_lib e.g.) and import from there and supply as argument to the loader. Also av_mode now needs to be specified explicitly

--- core_tools/drivers/M3102_firmware_loader.py
+++ core_tools/drivers/M3102_firmware_loader.py
@@ -1,33 +1,22 @@
-"""
-Standard firmware files for the digitizer of the V2 setup.
+from projects.keysight_measurement.M3102A import MODES
 
-CLEAN : normal image, revert to the normal AWG.
-AVG : special image with MAV and interator. A custom qcodes driver needs to be used for that.
-"""
-M3102A_CLEAN = "C:/V2_code/FPGA_Bitstreams/Digitizer_FW1.41/clean_4_41.sbp"
-M3102A_AVG = "C:/V2_code/FPGA_Bitstreams/Digitizer_FW1.41/averaging_firmware_1_41.sbp"
+def firmware_loader(dig, file, av_mode):
+    """
+    load a custom firmware image on board of the digitizer
 
-from core_tools.drivers.M3102A import MODES
+    Args:
+        dig <SD_dig (qcodes instrument) : digitzer object
+        file (string) : location where to find the new firmware
+    """
+    err = dig.SD_AIN.FPGAload(file)
+    if av_mode == 'normal':
+        dig.set_aquisition_mode(MODES.NORMAL)
+    elif av_mode == 'average':
+        dig.set_aquisition_mode(MODES.AVERAGE)
+    else:
+        raise ValueError("Average mode must be normal or average")
 
-def firmware_loader(dig, file):
-	"""
-	load a custom firmware image on board of the digitizer
-
-	Args:
-		dig <SD_dig (qcodes instrument) : digitzer object
-		file (string) : location where to find the new firmware
-	"""
-	err = dig.SD_AIN.FPGAload(file)
-	if file == M3102A_CLEAN:
-		dig.set_aquisition_mode(MODES.NORMAL)
-	else:
-		dig.set_aquisition_mode(MODES.AVERAGE)
-
-
-	if err == 0 :
-		print("Succesful upload of the firmware")
-	else:
-		print("upload failed, error code {} (see keysight manual for instructions)".format(err))
-
-if __name__ == '__main__':
-	firmware_loader(dig, M3102A_CLEAN)
+    if err == 0 :
+        print("Succesful upload of the firmware")
+    else:
+        print("upload failed, error code {} (see keysight manual for instructions)".format(err))
