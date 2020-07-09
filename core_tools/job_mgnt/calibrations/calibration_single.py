8bcd4978349c708da93a91514f736def50c015e5
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-07-09 10:28:03 +0200

    úpdates

--- core_tools/job_mgnt/calibrations/calibration_single.py
+++ core_tools/job_mgnt/calibrations/calibration_single.py
@@ -1,5 +1,5 @@
 from calibration_data import data_mgr
-
+import qcodes as qc
 class CalibrationError(Exception):
 	pass
 
@@ -21,16 +21,22 @@ def calibration_wrapper(cls, function):
 	return run_function
 
 class calibration_generic():
-	def __init__(self):
+	FAIL = 0
+	SUCCES = 1
+	station = qc.Station.default
+	
+	def __new__(self):
 		self.update_interval = 0 # 0 for do not update
 		self.auto_update = False # automatically rerun the last calibration after the update intercal exceeded
 		self.prioritize = True # first calibration or first measurement
 		self.dependencies = dep_mgr()
-		self.data_mgr = data_mgr()
+		self.data_mgr = data_mgr(self, 'todo')
 		
 		# iteration variables
 		self._N_rep = 0
 		self._n = 0
+
+		return self
 
 	def get_data(self, parameters ,set_vals = dict()):
 		self.data_mgr.get(set_vals)
@@ -40,15 +46,20 @@ class calibration_generic():
 
 	def reiterate(self, N=1):
 		'''
-		call this function in the 
+		call this function in to reiterature the same calibration N times.
 		'''
 		self._N_rep = N+1
 		self._n = 0
 
-
-
-
-def ExampleCal(calibration_generic):
+class ExampleCal(calibration_generic):
 	def __init__(self):
 		self.dependencies += my_cals.readout_of_dot_1
 		self.dependencies += (my_cals.readout_of_dot_2, my_cals.tc_res)
+
+		self.setters = ...
+		self.getters = ...
+
+if __name__ == '__main__':
+	test = ExampleCal()
+	print(test)
+	print(test.update_interval)
