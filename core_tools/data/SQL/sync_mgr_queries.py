18fca458c8a95894ce59491a9a3f65aec901ad38
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-11-03 09:16:04 +0100

    added sql (not debugged)

--- core_tools/data/SQL/sync_mgr_queries.py
+++ core_tools/data/SQL/sync_mgr_queries.py
@@ -1,5 +1,5 @@
 from core_tools.data.SQL.SQL_commands import write_query_generator
-
+from core_tools.data.SQL.connector import SQL_conn_info_remote
 class sync_mgr_query:
 	@staticmethod
 	def check_meas_4_upload():
@@ -55,17 +55,64 @@ class sync_mgr_query:
 		cur_rem.execute(stmt)
 		cur_rem.close()
 
-	def fill_measurement_table(meas_table_name):
+	def fill_and_sync_measurement_table(meas_table_name):
 		cur_rem = sync_agent().conn_remote.cursor()
 		cur_loc = sync_agent().conn_local.cursor()
 
 
-		statement_n_rows = "SELECT COUNT(*) FROM global_measurement_overview ;".format(uuid)
+		statement_n_rows = "SELECT COUNT(*) FROM {} ;".format(uuid, meas_table_name)
 
 		cur_rem.execute(statement_n_rows)
 		res_rem = cur_rem.fetchone()
 		cur_loc.execute(statement_n_rows)
-		cur_loc = cur_loc.fetchone()
+		res_loc = cur_loc.fetchone()
 
 		# check includes also a neglect of the full table
 		if res_rem[0] != res_loc[0]:
+			# drop
+			statement_get_rid_of_table = "DROP TABLE IF EXISTS {} ; ".format(meas_table_name)
+			cur_rem.execute(statement_get_rid_of_table)
+			
+			# create
+			cur_rem.execute(write_query_generator.make_new_data_table(meas_table_name))
+			
+			# copy table
+			statement_data_to_insert = "SELECT id, param_id, nth_set, nth_dim, param_id_m_param, setpoint, setpoint_local, name_gobal, name, label, unit, depencies, shape, total_size FROM {} ;".format(meas_table_name)
+
+			cur_loc.execute(statement_data_to_insert)
+			res_loc = cur_loc.fetchall()
+
+			for i in res_loc:
+				lobject = sync_agent().conn_remote.lobject(0,'w')
+				oid = lobject.oid
+				cursor = 0
+
+				statement_insert_row = "INSERT INTO {} (id, param_id, nth_set, nth_dim, param_id_m_param, setpoint, setpoint_local, name_gobal, name, label, unit, depencies, shape, total_size, write_cursor, oid) {} ;".format(meas_table_name, *i, write_cursor, oid)
+				cur_rem.execute(statement_n_rows)
+				sync_agent().conn_remote.commit()
+				lobject.close()
+
+		# write data from a to b
+
+		statement_write_status = "SELECT write_cursor, total_size, oid FROM {} ;".format(uuid, meas_table_name)
+
+		cur_rem.execute(statement_write_status)
+		res_rem = cur_rem.fetchone()
+		cur_loc.execute(statement_write_status)
+		cur_loc = cur_loc.fetchone()
+
+
+
+
+		for i in res_local:
+			write_cursor = i[0]
+			total_size = i[1]
+			oid = i[2]
+
+			if write_cursor == total_size:
+				stmnt = "UPDATE {} set (synchronized, sync_location) True, {} where oid={} ;".format(meas_table_name, 
+					str(SQL_conn_info_remote.dbname) + "@" + str(SQL_conn_info_remote.host), oid)
+				cur_rem.execute(stmnt)
+
+		cur_rem.close()
+		cur_loc.close()
