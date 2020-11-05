942b1fc639de35258b5e408b26d398faab923f9f
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-11-05 12:08:25 +0100

    added examples

--- sample_specific/station/load_station.py
+++
@@ -1,7 +0,0 @@
-
-from sample_specific.station import init_station
-from sample_specific.station.load_pulse_lib import return_pulse_lib
-import logging
-station = init_station()
-
-pulse = return_pulse_lib(station.hardware, station.AWG1, station.AWG2, station.AWG3)
