bdafe2a0149c0c3f246785995d59dc149cfd8552
Stephan Philips <s.g.j.philips@tudelft.nl>
2020-12-03 23:07:12 +0100

    fix station weird formatting

--- core_tools/sweeps/sweeps.py
+++ core_tools/sweeps/sweeps.py
@@ -214,8 +214,8 @@ if __name__ == '__main__':
     station.add_component(instr)
 
     from core_tools.data.SQL.connector import SQL_conn_info_local, SQL_conn_info_remote, sample_info, set_up_local_storage
-    set_up_local_storage('stephan', 'magicc', 'test', 'Intel Project', 'F006', 'SQ38328342')
-    # set_up_local_storage("xld_user", "XLDspin001", "vandersypen_data", "6dot", "XLD", "testing")
+    # set_up_local_storage('stephan', 'magicc', 'test', 'Intel Project', 'F006', 'SQ38328342')
+    set_up_local_storage("xld_user", "XLDspin001", "vandersypen_data", "6dot", "XLD", "testing")
 
     now = str(datetime.datetime.now())
     path = os.path.join(os.getcwd(), 'test.db')
@@ -231,7 +231,6 @@ if __name__ == '__main__':
     timer = ElapsedTimeParameter('time')
     my_param_multi_test =dummy_multi_parameter_2dawg('param')
 
-    from sweep_utility import sweep_info
 
     s = sweep_info(x,10,100,10,0)
     print(s.param(5))
@@ -255,6 +254,6 @@ if __name__ == '__main__':
     # do1D(x, 0,5,100, 0.01, my_param).run()
 
     # do2D(y, 0,5,100, 0.001,x, 0,5,100, 0.001, my_param).run()
-
-    print(station.snapshot())
+    # print(ds.snapshot)
+    # print(station.snapshot())
     
