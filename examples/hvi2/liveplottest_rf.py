6cf8e28fe87174a107b817ed1f0353178d996649
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2021-10-11 11:35:04 +0200

    Added phase configuration to keysight_rfgen

--- examples/hvi2/liveplottest_rf.py
+++ examples/hvi2/liveplottest_rf.py
@@ -1,7 +1,8 @@
 import time
 import logging
+import numpy as np
 from PyQt5 import QtCore
-
+from functools import partial
 import qcodes
 
 from core_tools.GUI.keysight_videomaps.liveplotting import liveplotting
@@ -12,10 +13,9 @@ from keysight_fpga.sd1.fpga_utils import \
     print_fpga_info, config_fpga_debug_log, print_fpga_log
 from keysight_fpga.sd1.dig_iq import load_iq_image
 from core_tools.HVI2.hvi2_schedule_loader import Hvi2ScheduleLoader
+from core_tools.drivers.keysight_rfgen import keysight_rfgen
 
 from pulse_lib.base_pulse import pulselib
-
-from PyQt5.QtCore import QTimer
 
 #start_all_logging()
 #logger.get_file_handler().setLevel(logging.DEBUG)
@@ -52,101 +52,72 @@ def create_pulse_lib(awgs):
     pulse.finish_init()
     return pulse
 
-class UpdateTimer(QtCore.QObject):
 
-    def start(self, plotting):
-        self.count = 0
-        self.timer = QTimer(plotting)
-        self.timer.timeout.connect(self.change_plot)
-        self.timer.start(9000)
-        plotting.destroyed.connect(self.stop_timer)
-
-    def stop_timer(self):
-        logging.info('Stop timer')
-        self.timer.stop()
-
-    def change_plot(self):
-        self.count += 1
-        logging.info(f'start #{self.count}')
-        v = (plotting._2D_V1_swing.value() + 1)
-        if v > 1100:
-            v = 100
-
-        plotting._2D_V1_swing.setValue(v)
-        start = time.monotonic()
-        plotting.update_plot_settings_2D()
-        logging.info(f'restart duration {time.monotonic()- start:5.2f} s')
-
-
-awg_slots = [3]
-dig_slot = 6
+awg_slots = [3,7]
+dig_slot = 5
 dig_mode = 2
 
 f1 = 190e6
 f2 = 110e6
-awg_channel_los = [('AWG1',1,0), ('AWG1',1,1), ('AWG1',3,0), ('AWG1',3,1)]
-awg_lo_amps = [600, 200]
-awg_lo_freq = [f1, f2]
+IF = 0
+amp1 = 600
+amp2 = 200
 
-dig_channels = [1,2,3,4]
-p2decim = 0
-lo_f = [0, 0, 0, f1, f2]
-input_ch = [0, 1, 2, 1, 1]
+# lo-key: AWG, AWG channel, amplitude, frequency, enable
+awg_channel_los = {
+    'lo1_0': ('AWG1', 1, amp1, f1, True),
+    'lo1_1': ('AWG1', 1, amp2, f2, True),
+    'lo3_0': ('AWG2', 3, amp1, f1, True),
+    'lo3_1': ('AWG2', 3, amp2, f2, True),
+    }
+
+# lo-key, DAQ channel, hw input channel, intermediate frequency, IF band (+/-1), phase
+dig_channel_los = {
+    ('lo1_0', 1, 1, IF, +1, 0.0),
+    ('lo1_1', 2, 1, IF, +1, 0.0),
+    ('lo3_0', 3, 3, IF, +1, 0.0),
+    ('lo3_1', 4, 3, IF, +1, 0.0),
+    }
+
+station = qcodes.Station()
 
 awgs = []
 for i,slot in enumerate(awg_slots):
     awg = M3202A_fpga(f"AWG{i+1}", 1, slot)
     awgs.append(awg)
     awg.set_digital_filter_mode(0)
-
-dig = SD_DIG("dig", 1, 6)
-
-station = qcodes.Station()
-station_name = 'Test'
-
-for awg in awgs:
     station.add_component(awg)
 
+dig = SD_DIG("dig", 1, 5)
 station.add_component(dig)
 
 load_iq_image(dig.SD_AIN)
 print_fpga_info(dig.SD_AIN)
 dig.set_acquisition_mode(dig_mode)
 
-for awg in awgs:
-    for awg_name, channel, lo in awg_channel_los:
-        if awg_name == awg.name:
-            awg.config_lo(channel, lo, True, awg_lo_freq[lo], awg_lo_amps[lo])
-            awg.set_lo_mode(channel, True)
-
-for ch in dig_channels:
-    dig.set_lo(ch, lo_f[ch], 0, input_channel=input_ch[ch])
-
 
 logging.info('init pulse lib')
 # load the AWG library
 pulse = create_pulse_lib(awgs)
 
+rf_gen = keysight_rfgen('keysight_rfgen', awg_channel_los, pulse.awg_devices,
+                        station.dig, dig_channel_los)
+station.add_component(rf_gen)
+
 print('start gui')
 
 logging.info('open plotting')
-plotting = liveplotting(pulse, dig, "Keysight", iq_mode={1:'I', 2:'I', 3:'angle_deg', 4:'angle_deg'})
+channel_map = {
+        'ch1_I':(1, np.real),
+        'ch1_Q':(1, np.imag),
+        'ch1_Amp':(1, np.abs),
+        'ch1_Phase':(1, partial(np.angle, deg=True)),
+        'ch2_Amp':(2, np.abs),
+        'ch2_Phase':(2, np.angle),
+    }
+plotting = liveplotting(pulse, dig, "Keysight", channel_map=channel_map)
 plotting._2D_gate2_name.setCurrentIndex(1)
 plotting._2D_t_meas.setValue(1)
 plotting._2D_V1_swing.setValue(100)
 plotting._2D_npt.setValue(80)
 
-#updateTimer = UpdateTimer()
-#updateTimer.start(plotting)
-#plotting._2D_start_stop()
-
-#%%
-
-# station.close_all_registered_instruments()
-
-#from V2_software.LivePlotting.data_getter.scan_generator_Virtual import fake_digitizer
-#
-#dig = fake_digitizer("fake_digitizer")
-#pulse = return_pulse_lib_quad_dot(None)
-#
-#V2_liveplotting(pulse, dig)
