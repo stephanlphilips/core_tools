71180f1de87a4f91eabfafb9fd713e7323d6ac62
Stephan Philips <s.g.j.philips@tudelft.nl>
2022-02-01 20:05:52 +0100

    added functions for dumpin/loading ddata from the hard drive

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
-packages = ['core_tools', 'sample_specific', 'keysightSD1']
-print('packages: {}'.format(packages))
-
 setup(name="core_tools",
 	version="1.0",
 	packages = find_packages(),
