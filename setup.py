3670316a40a6dc7a8ae00be59c74d1cc859dbb11
Stephan Philips <s.g.j.philips@tudelft.nl>
2022-02-18 11:36:52 +0100

    added setup with current tested versions

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ setup(name="core_tools",
 	version="1.0",
 	packages = find_packages(),
 	install_requires=[
-          'pyqtgraph','si-prefix', 'matplotlib', 'psycopg2 '
+          'pyqtgraph==0.12.3',
+          'si-prefix==1.2.2',
+          'matplotlib==3.5.1',
+          'psycopg2==2.9.3',
+          'h5py==3.6.0',
+          'qcodes==0.32.0',
+          'pyqt5==5.15.6',
+          'lmfit==1.0.3',
+          'qutip==4.6.3'
       ],
 	)
