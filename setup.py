adc1e09d28f2cb4f38c9b52b78875fb49a1a6590
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-04-14 13:32:46 +0200

    Improved setup and release

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(name="core_tools",
 	version="1.1.2",
 	packages = find_packages(),
+    python_requires=">=3.7",
 	install_requires=[
-          'pyqtgraph','si-prefix', 'matplotlib', 'psycopg2 '
+          'pyqtgraph','si-prefix', 'matplotlib', 'psycopg2',
+          'pulse_lib',
       ],
 	)
