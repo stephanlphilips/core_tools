18799e9a2acf96761305f55ae54725d7b55a8ced
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-04-14 13:23:50 +0200

    Added qt_init() for reliable startup of GUIs

--- core_tools/GUI/qt_util.py
+++ core_tools/GUI/qt_util.py
@@ -1,10 +1,21 @@
 import logging
 import threading
 
+from PyQt5 import QtCore, QtWidgets
+
 is_wrapped = threading.local()
 is_wrapped.val = False
 
 def qt_log_exception(func):
+    ''' Decorator to log exceptions.
+    Exceptions are logged and raised again.
+    Decorator is designed to be used around functions being called as
+    QT event handlers, because QT doesn't report the exceptions.
+    Note:
+        The decorated method/function cannot be used with
+        functools.partial.
+    '''
+
     def wrapped(*args, **kwargs):
         if is_wrapped.val:
             return func(*args, **kwargs)
@@ -18,5 +29,21 @@ def qt_log_exception(func):
             finally:
                 is_wrapped.val = False
 
-
     return wrapped
+
+
+_qt_app = None
+
+def qt_init():
+    '''Starts the QT application if not yet started.
+    Most of the cases the QT backend is already started
+    by IPython, but sometimes it is not.
+    '''
+    # application reference must be held in global scope
+    global _qt_app
+    _qt_app = QtCore.QCoreApplication.instance()
+    if _qt_app is None:
+        logging.info('Create Qt application')
+        _qt_app = QtWidgets.QApplication([])
+    else:
+        logging.debug('Qt application already created')
