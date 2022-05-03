760802c1f9eb5173a30793b173a6249d87914309
dcrielaard <damiencrielaard@gmail.com>
2022-05-03 09:26:57 +0200

    remove constructor from interface

--- core_tools/GUI/keysight_videomaps/data_saver/data_saver.py
+++ core_tools/GUI/keysight_videomaps/data_saver/data_saver.py
@@ -1,23 +1,11 @@
-from typing import Union, Any, Tuple, Dict, Optional
-
+from typing import Any, Tuple, Dict
 from abc import ABC, abstractmethod
-
-from pathlib import Path
 
 from qcodes import MultiParameter
 
 
 class IDataSaver(ABC):
     """Specifies the interface data savers are to adhere to."""
-
-    @abstractmethod
-    def __init__(self, path: Optional[Union[Path, str]] = None):
-        """
-        Constructor.
-
-        Args:
-            path: Specifies where to store the data. If left None, the default for the data saver is used.
-        """
 
     @abstractmethod
     def save_data(self, vm_data_parameter: MultiParameter, label: str) -> Tuple[Any, Dict[str, Any]]:
