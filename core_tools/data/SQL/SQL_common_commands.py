6049358d51cf7dba4fc093bed5f385eb0593673f
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2022-08-08 09:51:58 +0200

    Improved database exception handling: close and restart connection

--- core_tools/data/SQL/SQL_common_commands.py
+++ core_tools/data/SQL/SQL_common_commands.py
@@ -4,115 +4,126 @@ from psycopg2 import sql
 from core_tools.data.SQL.SQL_utility import sql_name_formatter, sql_value_formatter, name_value_formatter
 
 def execute_statement(conn, statement, placeholders = []):
-	cursor = conn.cursor()
-	cursor.execute(statement, placeholders)
-	cursor.close()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(statement, placeholders)
+        cursor.close()
+        return ((), )
+    except:
+        # After exception the connection cannot be used anymore.
+        # A new connection will automatically be opened for the next command.
+        conn.close()
+        raise
 
-	return ((), )
 
 def execute_query(conn, query, dict_cursor=False, placeholders = []):
-	if dict_cursor == False:
-		cursor = conn.cursor()
-	else:
-		cursor = conn.cursor(cursor_factory=RealDictCursor)
-	
-	cursor.execute(query, placeholders)
+    try:
+        if dict_cursor == False:
+            cursor = conn.cursor()
+        else:
+            cursor = conn.cursor(cursor_factory=RealDictCursor)
 
-	return_values =cursor.fetchall()
-	cursor.close()
-	
-	return return_values
+        cursor.execute(query, placeholders)
+        return_values = cursor.fetchall()
+        cursor.close()
+        return return_values
+    except:
+        # After exception the connection cannot be used anymore.
+        # A new connection will automatically be opened for the next command.
+        conn.close()
+        raise
+
 
 def select_elements_in_table(conn, table_name, var_names, where=None, order_by = None, limit=None, dict_cursor=True):
-	'''
-	execute a query on a table
+    '''
+    execute a query on a table
 
-	Args:
-		conn (psycopg2.connect) : connection object from psycopg2 librabry
-		table_name (str) : name of the table to update
-		var_names (tuple<str>) : variable names of the table
-		where (str) : selection critere (e.g. 'id = 5')
-		order_by (tuple, str) : order results (e.g. ('uuid',  'DESC')
-		limit (int) : limit the amount of results
-		dict_cursor (bool) : return result as an ordered dict
-	'''
-	var_names_SQL = sql_name_formatter(var_names)
+    Args:
+        conn (psycopg2.connect) : connection object from psycopg2 librabry
+        table_name (str) : name of the table to update
+        var_names (tuple<str>) : variable names of the table
+        where (str) : selection critere (e.g. 'id = 5')
+        order_by (tuple, str) : order results (e.g. ('uuid',  'DESC')
+        limit (int) : limit the amount of results
+        dict_cursor (bool) : return result as an ordered dict
+    '''
+    var_names_SQL = sql_name_formatter(var_names)
 
-	query = sql.SQL("select {0} from {1} ").format(
-				sql.SQL(', ').join(var_names_SQL),
-				sql.SQL(table_name))
-	# SQL.Identifier does not work with underscore names for tables?
+    query = sql.SQL("select {0} from {1} ").format(
+                sql.SQL(', ').join(var_names_SQL),
+                sql.SQL(table_name))
+    # SQL.Identifier does not work with underscore names for tables?
 
-	if where is not None:
-		query += sql.SQL("WHERE {0} = {1} ").format(sql.Identifier(where[0]), sql.Literal(where[1]))
-	if order_by is not None:
-		query += sql.SQL("ORDER BY {0} {1} ").format(sql.Identifier(order_by[0]), sql.SQL(order_by[1]))
-	if limit is not None:
-		query += sql.SQL("LIMIT {0} ").format(sql.SQL(str(int(limit))))
+    if where is not None:
+        query += sql.SQL("WHERE {0} = {1} ").format(sql.Identifier(where[0]), sql.Literal(where[1]))
+    if order_by is not None:
+        query += sql.SQL("ORDER BY {0} {1} ").format(sql.Identifier(order_by[0]), sql.SQL(order_by[1]))
+    if limit is not None:
+        query += sql.SQL("LIMIT {0} ").format(sql.SQL(str(int(limit))))
 
-	return execute_query(conn, query, dict_cursor)
+    return execute_query(conn, query, dict_cursor)
 
 def insert_row_in_table(conn, table_name, var_names, var_values, returning=None, custom_statement=''):
-	'''
-	insert a row in a table
+    '''
+    insert a row in a table
 
-	Args:
-		conn (psycopg2.connect) : connection object from psycopg2 librabry
-		table_name (str) : name of the table to update
-		var_names (tuple<str>) : variable names of the table
-		var_values (tuple<str>) : values corresponding to the variable names
-		returning (tuple<str>) : name of a variables you want returned
-	'''
-	var_values_SQL, placeholders = sql_value_formatter(var_values)
-	var_names_SQL = sql_name_formatter(var_names)
+    Args:
+        conn (psycopg2.connect) : connection object from psycopg2 librabry
+        table_name (str) : name of the table to update
+        var_names (tuple<str>) : variable names of the table
+        var_values (tuple<str>) : values corresponding to the variable names
+        returning (tuple<str>) : name of a variables you want returned
+    '''
+    var_values_SQL, placeholders = sql_value_formatter(var_values)
+    var_names_SQL = sql_name_formatter(var_names)
 
-	statement = sql.SQL("INSERT INTO {} ({}) VALUES ({}) ").format(sql.SQL(table_name),
-			sql.SQL(', ').join(var_names_SQL),
-			sql.SQL(', ').join(var_values_SQL))
+    statement = sql.SQL("INSERT INTO {} ({}) VALUES ({}) ").format(sql.SQL(table_name),
+            sql.SQL(', ').join(var_names_SQL),
+            sql.SQL(', ').join(var_values_SQL))
 
-	if returning is None:
-		return execute_statement(conn, statement + sql.SQL(custom_statement), placeholders)
-	else:
-		statement += sql.SQL(" RETURNING {} ").format(sql.SQL(", ").join([sql.Identifier(i) for i in returning]))
-		return execute_query(conn, statement + sql.SQL(custom_statement), placeholders=placeholders)
+    if returning is None:
+        return execute_statement(conn, statement + sql.SQL(custom_statement), placeholders)
+    else:
+        statement += sql.SQL(" RETURNING {} ").format(sql.SQL(", ").join([sql.Identifier(i) for i in returning]))
+        return execute_query(conn, statement + sql.SQL(custom_statement), placeholders=placeholders)
 
 
 def update_table(conn, table_name, var_names, var_values, condition=None):
-	'''
-	generate statement for updating an existing stable
+    '''
+    generate statement for updating an existing stable
 
-	Args:
-		conn (psycopg2.connect) : connection object from psycopg2 librabry
-		table_name (str) : name of the table to update
-		var_names (tuple<str>) : variable names of the table
-		var_values (tuple<str>) : values corresponding to the variable names
-		condition (str) : condition for the update (e.g. 'id = 5')
-	'''
+    Args:
+        conn (psycopg2.connect) : connection object from psycopg2 librabry
+        table_name (str) : name of the table to update
+        var_names (tuple<str>) : variable names of the table
+        var_values (tuple<str>) : values corresponding to the variable names
+        condition (str) : condition for the update (e.g. 'id = 5')
+    '''
 
-	statement = sql.SQL("UPDATE {} SET ").format(sql.SQL(table_name))
+    statement = sql.SQL("UPDATE {} SET ").format(sql.SQL(table_name))
 
-	names_values = name_value_formatter(var_names, var_values)
-	if len(names_values) == 0:
-		return ""
+    names_values = name_value_formatter(var_names, var_values)
+    if len(names_values) == 0:
+        return ""
 
-	statement += sql.SQL(', ').join(sql.SQL("{} = {} ").format(i,j) for i,j in names_values.var_name_pairs)
+    statement += sql.SQL(', ').join(sql.SQL("{} = {} ").format(i,j) for i,j in names_values.var_name_pairs)
 
-	if condition is not None:
-		statement += sql.SQL("WHERE {0} = {1} ").format(sql.Identifier(condition[0]), sql.Literal(condition[1]))
+    if condition is not None:
+        statement += sql.SQL("WHERE {0} = {1} ").format(sql.Identifier(condition[0]), sql.Literal(condition[1]))
 
-	return execute_statement(conn, statement, placeholders=names_values.placeholders)
+    return execute_statement(conn, statement, placeholders=names_values.placeholders)
 
 def alter_table(conn, table_name, colums, dtypes):
-	'''
-	add columns to a table
+    '''
+    add columns to a table
 
-	Args:
-		conn (psycopg2.connect) : connection object from psycopg2 librabry
-		table_name (str) : name of the table to update
-		colums (tuple<str>) : names of the columns
-		dtypes (tuple<str>) : type of the column's
-	'''
-	statement = sql.SQL("ALTER TABLE {} ADD COLUMN ").format(sql.SQL(table_name))
-	statement += sql.SQL(" , ADD COLUMN ").join(sql.SQL(" {0} {1} ").format(sql.Identifier(i), sql.SQL(j)) for i, j in zip(colums, dtypes))
+    Args:
+        conn (psycopg2.connect) : connection object from psycopg2 librabry
+        table_name (str) : name of the table to update
+        colums (tuple<str>) : names of the columns
+        dtypes (tuple<str>) : type of the column's
+    '''
+    statement = sql.SQL("ALTER TABLE {} ADD COLUMN ").format(sql.SQL(table_name))
+    statement += sql.SQL(" , ADD COLUMN ").join(sql.SQL(" {0} {1} ").format(sql.Identifier(i), sql.SQL(j)) for i, j in zip(colums, dtypes))
 
-	return execute_statement(conn, statement)
+    return execute_statement(conn, statement)
