e6ec54d3ea2afe64b584aa50d0d03ed05bb6fcee
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2022-09-26 15:26:04 +0200

    Bump version: 1.4.3 → 1.4.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="core_tools",
-	version="1.4.3",
+	version="1.4.4",
 	packages = find_packages(),
     python_requires=">=3.7",
 	install_requires=[
