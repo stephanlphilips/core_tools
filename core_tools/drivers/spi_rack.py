83919efdccf8ea4f3753f5f40fe0b0a798fc00f2
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2022-11-20 15:30:48 +0100

    Added get_idn

--- core_tools/drivers/spi_rack.py
+++ core_tools/drivers/spi_rack.py
@@ -3,7 +3,13 @@ from qcodes.instrument.base import Instrument
 import spirack
 
 class SPI_rack(Instrument):
-	def __init__(self, name, address, baud_rate='115200', timeout=1):
-		super().__init__(name)
-		self.spi_rack = spirack.SPI_rack(address, baud=baud_rate, timeout=timeout)
-		self.spi_rack.unlock()
+    def __init__(self, name, address, baud_rate='115200', timeout=1):
+        super().__init__(name)
+        self.spi_rack = spirack.SPI_rack(address, baud=baud_rate, timeout=timeout)
+        self.spi_rack.unlock()
+
+    def get_idn(self):
+        return dict(vendor='CoreTools',
+                    model='SPI_rack',
+                    serial='',
+                    firmware='')
