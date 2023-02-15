0321f84f8428e0e0de6fdb43be14f9cf11d7c2dc
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-02-15 13:54:46 +0100

    Do not use patched Keysight SD1 code with Python > 3.7

--- keysightSD1/__init__.py
+++ keysightSD1/__init__.py
@@ -6,6 +6,10 @@ try:
 
 except:
     # import the patched core_tools version of keysightSD1 v3.1
+    import sys
+    if sys.version_info.minor > 7:
+        raise Exception("Please install official KeysightSD1 driver. "
+                        "Core-tools driver does not support Python > 3.7")
     from .keysightSD1_31 import *
 
     __version__ = '3.1'
