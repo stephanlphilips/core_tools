908093caadb105452b77673704272da6b4246957
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-02-15 13:04:16 +0100

    Added test code for ScriptRunner

---
+++ core_tools/GUI/script_runner/test_script.py
@@ -0,0 +1,24 @@
+
+'''
+Script with some cells to test and demonstrate ScriptRunner
+'''
+
+#%% Say Hi
+
+# This cell can be called by name "Say Hi"
+
+print('Hi')
+
+#%%
+
+print('Hello')
+
+
+#%% Oops
+
+raise Exception("Oops")
+
+#%% Syntax Error
+
+# intentional syntax error for test!
+return
