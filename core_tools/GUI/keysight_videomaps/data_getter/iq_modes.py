7cf99b2a3ead67da15286fb93e422d4e4e71c3d6
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-03-02 13:40:29 +0100

    corrected channel name for phase result

--- core_tools/GUI/keysight_videomaps/data_getter/iq_modes.py
+++ core_tools/GUI/keysight_videomaps/data_getter/iq_modes.py
@@ -7,9 +7,9 @@ iq_mode2numpy = {
         'phase': np.angle,
         'phase_deg': lambda x:np.angle(x, deg=True),
         'I+Q': [('_I', np.real), ('_Q', np.imag)],
-        'amplitude+phase': [('_amp', np.abs), ('_angle', np.angle)],
+        'amplitude+phase': [('_amp', np.abs), ('_phase', np.angle)],
         'abs': np.abs,
         'angle': np.angle,
         'angle_deg': lambda x:np.angle(x, deg=True),
-        'abs+angle': [('_amp', np.abs), ('_angle', np.angle)],
+        'abs+angle': [('_amp', np.abs), ('_phase', np.angle)],
         }
