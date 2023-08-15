d3c9386db1a747b161f984a72b8941924ea85d87
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-08-15 16:26:48 +0200

    Check whether file exists before trying to open with xarray

--- core_tools/data/ds/ds_hdf5.py
+++ core_tools/data/ds/ds_hdf5.py
@@ -6,6 +6,14 @@ import os
 def _get_fname(uuid):
     return f'ds_{uuid}.hdf5'
 
+def _load_xr_hdf5(fname):
+    # Check existence. xarray gives a very confusing error when the file does not exist.
+    if not os.path.exists(fname):
+        raise FileNotFoundError(fname)
+    xds = xr.open_dataset(fname)
+    xds.close()
+    return xds
+
 def save_hdf5(ds, fname):
     xds = ds2xarray(ds)
     comp = {"compression": "gzip", "compression_opts": 9}
@@ -13,9 +21,8 @@ def save_hdf5(ds, fname):
     xds.to_netcdf(fname, engine='h5netcdf', encoding=encoding)
 
 def load_hdf5(fname):
-    xs = xr.open_dataset(fname)
+    xs = _load_xr_hdf5(fname)
     ds = xarray2ds(xs)
-    xs.close()
     return ds
 
 def save_hdf5_uuid(ds, path):
@@ -32,9 +39,7 @@ def load_hdf5_uuid(uuid, path):
 def load_xr_by_uuid(uuid, path):
     name = _get_fname(uuid)
     fname = os.path.join(path, name)
-    xds = xr.open_dataset(fname)
-    xds.close()
-    return xds
+    return _load_xr_hdf5(fname)
 
 def save_hdf5_id(ds, path):
     os.makedirs(path, exist_ok=True)
@@ -50,7 +55,5 @@ def load_hdf5_id(id, path):
 def load_xr_by_id(id, path):
     name = _get_fname(id)
     fname = os.path.join(path, name)
-    xds = xr.open_dataset(fname)
-    xds.close()
-    return xds
+    return _load_xr_hdf5(fname)
 
