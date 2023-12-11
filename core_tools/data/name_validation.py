ff9c6da29a796e1c628929649489131576b51903
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2023-12-11 15:08:45 +0100

    Improved error message

--- core_tools/data/name_validation.py
+++ core_tools/data/name_validation.py
@@ -2,7 +2,7 @@ import re
 
 def validate_dataset_name(name):
     if len(name) < 2:
-        raise Exception(f"Dataset name {name} is too short. Min length is 2 chars")
+        raise Exception(f"Dataset name '{name}' is too short. Min length is 2 chars")
     if len(name) > 100:
         raise Exception("Dataset name is too long. Max is 100")
     if '{' in name:
@@ -12,9 +12,9 @@ def validate_dataset_name(name):
 
 def validate_data_identifier_value(value):
     if len(value) < 1:
-        raise Exception(f"Name {value} is too short. ")
+        raise Exception(f"Name '{value}' is too short. ")
     if len(value) > 30:
-        raise Exception("Name {value} is too long. Max is 25")
+        raise Exception("Name '{value}' is too long. Max is 25")
     if '{' in value:
         raise Exception(f"Illegal name. Did you forget the f in front of the string? {value}")
     if not re.match(r"^[A-Za-z][A-Za-z0-9_\-.,:()[\]* ]*$", value):
