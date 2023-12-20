9aa4ceb49ee66a8c9ab465d2af4199841ebccf1c
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2023-12-20 09:36:19 +0100

    Improved error message

--- core_tools/data/name_validation.py
+++ core_tools/data/name_validation.py
@@ -6,7 +6,7 @@ def validate_dataset_name(name):
     if len(name) > 100:
         raise Exception("Dataset name is too long. Max is 100")
     if '{' in name:
-        raise Exception(f"Illegal name. Did you forget the f in front of the string? {name}")
+        raise Exception(f"Illegal name '{name}'. Did you forget the f in front of the string?")
     if not re.match(r"^[A-Za-z0-9_\-.,:()[\]*+&/ ]*$", name):
         raise Exception(f"Invalid dataset name {name}")
 
@@ -16,7 +16,7 @@ def validate_data_identifier_value(value):
     if len(value) > 30:
         raise Exception("Name '{value}' is too long. Max is 25")
     if '{' in value:
-        raise Exception(f"Illegal name. Did you forget the f in front of the string? {value}")
+        raise Exception(f"Illegal name '{value}'. Did you forget the f in front of the string?")
     if not re.match(r"^[A-Za-z][A-Za-z0-9_\-.,:()[\]* ]*$", value):
         raise Exception(f"Invalid name {value}")
 
