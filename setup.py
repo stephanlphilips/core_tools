9322489f38a6d59ec594e4604a0006a573ecb161
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2024-01-12 15:20:05 +0100

    Bump to 1.4.39

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="core_tools",
-    version="1.4.38",
+    version="1.4.39",
     packages = find_packages(),
     python_requires=">=3.7",
     install_requires=[
