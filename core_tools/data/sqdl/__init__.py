1812c6dde866fbc4061287d33d0f8f89ea5c2fc6
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2024-09-09 14:24:49 +0200

    Added sqdl_query, sqdl_logout, load_uuids_parallel

---
+++ core_tools/data/sqdl/__init__.py
@@ -0,0 +1,8 @@
+from .sqdl_reader import (
+    init_sqdl,
+    sqdl_logout,
+    list_scopes,
+    load_by_uuid,
+    load_uuids_parallel,
+    sqdl_query,
+    )
--- core_tools/data/sqdl/sqdl_reader.py
+++ core_tools/data/sqdl/sqdl_reader.py
@@ -1,25 +1,43 @@
 import io
 import requests
 import xarray as xr
-from typing import Optional, Union
+from datetime import datetime
+from dataclasses import dataclass
+from multiprocessing.pool import ThreadPool
 
+from tqdm import tqdm
 from sqdl_client.client import QDLClient
 from core_tools.data.ds.data_set import data_set
 from core_tools.data.ds.xarray2ds import xarray2ds
 
 _DATASET_READER = None
 
+
+@dataclass
+class DatasetInfo:
+    uuid: str
+    name: str
+    start_time: datetime
+    setup: str
+    sample: str
+    starred: bool
+    variables: list[str]
+    axes: list[str]
+
+
 class DatasetReader:
-    def __init__(self, scope_name: Optional[str] = None):
+    def __init__(self, scope_name: str | None = None):
         client = QDLClient()
         self.client = client
         client.login()
         self.s3_session = requests.Session()
         if scope_name:
-            sqdl_api = client.api
-            self.scope = sqdl_api.scope.retrieve_from_name(scope_name)
+            self.set_scope(scope_name)
         else:
             self.scope = None
+
+    def logout(self):
+        self.client.logout()
 
     def set_scope(self, scope_name: str):
         sqdl_api = self.client.api
@@ -53,13 +71,99 @@ class DatasetReader:
         with io.BytesIO(resp.content) as fp:
             return xr.load_dataset(fp)
 
+    def query_datasets(
+            self,
+            name_contains: str = None,
+            start_time: datetime = None,
+            end_time: datetime = None,
+            setup: str = None,
+            sample: str = None,
+            starred: bool = False,
+            variables: list[str] | str | None = None,
+            axes: list[str] | str | None = None,
+            ) -> list[DatasetInfo]:
+        data_identifiers = {}
+        if sample is not None:
+            data_identifiers['sample'] = sample
+        if setup is not None:
+            data_identifiers['setup'] = setup
+        if variables is not None:
+            data_identifiers['variables_measured'] = variables
+        if axes is not None:
+            data_identifiers['dimensions'] = axes
 
-def init_sqdl(scope_name: str):
+        sqdl_res = self.scope.search_datasets(
+            dataset_name_contains = name_contains,
+            collected_since = start_time,
+            collected_until = end_time,
+            rating = 1 if starred else None,
+            data_identifiers = data_identifiers,
+            limit=1000,
+            )
+
+        res = []
+        for r in sqdl_res:
+            res.append(
+                DatasetInfo(
+                    r.uid,
+                    r.name,
+                    r.date_collected,
+                    r.metadata['setup'],
+                    r.metadata['sample'],
+                    r.rating > 0,
+                    r.metadata['variables_measured'],
+                    r.metadata['dimensions'],
+                    )
+                )
+
+        return res
+
+
+def _get_dataset_reader():
     global _DATASET_READER
     if _DATASET_READER is None:
-        _DATASET_READER = DatasetReader(scope_name)
+        _DATASET_READER = DatasetReader()
+    return _DATASET_READER
+
+
+def init_sqdl(scope_name: str):
+    _get_dataset_reader().set_scope(scope_name)
+
+
+def sqdl_logout():
+    """Logout sQDL.
+    The login window will popup when a new request to sQDL is made.
+    """
+    global _DATASET_READER
+    if _DATASET_READER is not None:
+        _DATASET_READER.logout()
     else:
-        _DATASET_READER.set_scope(scope_name)
+        client = QDLClient()
+        client.logout()
+
+
+def sqdl_query(
+            name_contains: str = None,
+            start_time: datetime = None,
+            end_time: datetime = None,
+            setup: str = None,
+            sample: str = None,
+            starred: bool = False,
+            variables: list[str] | str | None = None,
+            axes: list[str] | str | None = None,
+            ) -> list[DatasetInfo]:
+
+    return _get_dataset_reader().query_datasets(
+            name_contains=name_contains,
+            start_time=start_time,
+            end_time=end_time,
+            setup=setup,
+            sample=sample,
+            starred=starred,
+            variables=variables,
+            axes=axes,
+        )
+
 
 def list_scopes():
     global _DATASET_READER
@@ -68,8 +172,15 @@ def list_scopes():
     return _DATASET_READER.list_scopes()
 
 
-def load_by_uuid(uuid: Union[str, int]):
-    if _DATASET_READER is None or _DATASET_READER.scope is None:
+def load_by_uuid(uuid: str | int):
+    reader = _get_dataset_reader()
+    if reader.scope is None:
         raise Exception("sQDL connection must be configured with init_sqdl(scope_name)")
-    reader = _DATASET_READER
     return reader.load_ds_by_uuid(uuid)
+
+
+def load_uuids_parallel(uuids: list[int | str], print_progress=True):
+    iterator = tqdm(uuids) if print_progress else uuids
+    with ThreadPool() as pool:
+        return list(pool.map(load_by_uuid, iterator))
+
