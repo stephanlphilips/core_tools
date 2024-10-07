9c593424667fc01d23ff5c23330915d68cff596f
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2024-10-07 14:58:28 +0200

    Bump to 1.5.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="core_tools",
-    version="1.4.67",
+    version="1.5.0",
     packages = find_packages(),
     python_requires=">=3.10",
     install_requires=[
